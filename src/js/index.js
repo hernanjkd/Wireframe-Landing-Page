2b10923f28d32bb39d17cb7d1a6cb4c4322f4ddd
Alejandro Sanchez <aalejo@gmail.com>
2018-10-05 03:35:13 +0000

    added automatic prettyfier

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,15 @@
 //import react into the bundle
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
-import 'bootstrap';
+import "bootstrap";
 
 //include your index.scss file into the bundle
-import '../styles/index.scss';
+import "../styles/index.scss";
 
 //import your own components
-import {Home} from './component/home.jsx';
+import { Home } from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(
-    <Home />,
-    document.querySelector('#app')
-);
+ReactDOM.render(<Home />, document.querySelector("#app"));
