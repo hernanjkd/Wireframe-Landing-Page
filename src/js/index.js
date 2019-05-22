1079988fb7a5e6350e2d445dfc54af04ddf29843
ProspersMartin <martinprospers1@gmail.com>
2019-05-22 21:00:31 +0000

    created the navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,7 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
 import "bootstrap";
-
-//include your index.scss file into the bundle
 import "../styles/index.scss";
+import Home from "./component/home.js";
 
-//import your own components
-import { Home } from "./component/home.js";
-
-//render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
