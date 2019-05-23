dfe705cb262375215bc491be1bd21f430990389d
NKMillsaps <nkmillsaps@gmail.com>
2019-05-23 16:09:40 +0000

    Added Jumbotron - first vers

--- src/js/component/about.js
+++ src/js/component/about.js
@@ -5,7 +5,25 @@ class About extends React.Component {
 	render() {
 		return (
 			<div className="row">
-				<p className="card-text">About Us.</p>
+				<div className="jumbotron">
+					<h1 className="display-4">Hello, world!</h1>
+					<p className="lead">
+						This is a simple hero unit, a simple jumbotron-style
+						component for calling extra attention to featured
+						content or information.
+					</p>
+					<hr className="my-4" />
+					<p>
+						It uses utility classes for typography and spacing to
+						space content out within the larger container.
+					</p>
+					<a
+						className="btn btn-primary btn-lg"
+						href="#"
+						role="button">
+						Learn more
+					</a>
+				</div>
 			</div>
 		);
 	}
